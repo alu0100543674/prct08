48dec41c54adda7fa49b9e9344f8ba91bc259a02
Carla Hernandez <alu0100543674@ull.edu.es>
2013-11-08 11:26:36 +0000

    Modificado before de matriz_spec.rb

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,2 +1,8 @@
 class Matriz
+  attr_accesor :filas, :columnas, :matriz
+
+  def initialize(filas, columnas)
+     @filas, @columnas = filas, columnas
+  end
+  
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,15 +1,26 @@
 require 'matriz.rb'
 
 describe Matriz do
-   before :each do
-      @matriz1 = Matriz.new(2,2)
-      @matriz1[0] = [1, 2]
-      @matriz1[1] = [3, 4]
+  before :each do
+    @matriz1 = Matriz.new(2,2,[[1,1],[1, 1]])
+    # @matriz1[0] = [1,2]
+    # @matriz1[1] = [3,4]
+                
+    @matriz2 = Matriz.new(2,2,[[1,1],[1, 1]])
+    # @matriz2[0] = [1,2]
+    # @matriz2[1] = [3,4]
 
-      @matriz2 = Matriz.new(2,2)
-      @matriz2[0] = [5, 6]
-      @matriz2[1] = [7, 8]
+    @matriz3 = Matriz.new(2, 3, [[1,2,3], [4,5,6]])   
 
-   end
+  end
+
+   describe " Almacenamiento " do
+     it "Debe existir un numerador" do
+        @matriz1.filas.should eq(2)
+     end
+     it "Debe existir un denominador" do
+        @matriz1.columnas.should eq(2)
+     end
+  end
 
 end
